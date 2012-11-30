102de8de0c1a602cc6af51c80a94b5593f91084c
aisk <aisk1988@gmail.com>
2012-11-30 21:54:43 +0800

    some fix

--- pie/builtin.py
+++ pie/builtin.py
@@ -1,3 +1,4 @@
+import operator
 
 class Symbol(str):
     pass
@@ -8,3 +9,13 @@ sym_if = Symbol('if')
 sym_set = Symbol('set!')
 sym_quote = Symbol('quote')
 sym_begin = Symbol('begin')
+
+sym_begin = Symbol('import')
+
+add = sum
+
+sub = lambda *args: reduce(operator.sub, args)
+
+mul = lambda *args: reduce(operator.mul, args)
+
+div = lambda *args: reduce(operator.div, args)
